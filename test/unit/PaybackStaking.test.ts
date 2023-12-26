02dad61881dd88abee0b2897b41cc9fef9fa5967
reddigszymon <mixowo@hotmail.com>
2023-12-26 18:41:48 +0100

    Added constructor tests

--- test/unit/PaybackStaking.test.ts
+++ test/unit/PaybackStaking.test.ts
@@ -2,11 +2,16 @@ import { assert, expect } from "chai";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { network, deployments, ethers } from "hardhat";
 import { developmentChains } from "../../helper-hardhat-config";
-import { ContractManager, PaybackStaking } from "../../typechain-types";
+import {
+  ContractManager,
+  MockToken,
+  PaybackStaking,
+} from "../../typechain-types";
 import { agreementTerms, services, parties } from "../../utils/exampleContract";
 
 describe("PaybackStaking", function () {
   let paybackStaking: PaybackStaking;
+  let mockToken: MockToken;
   let deployer: SignerWithAddress;
   let player: SignerWithAddress;
   let playerTwo: SignerWithAddress;
@@ -19,11 +24,35 @@ describe("PaybackStaking", function () {
     deployer = accounts[0];
     player = accounts[1];
     playerTwo = accounts[2];
-    await deployments.fixture(["contract_manager"]);
+    await deployments.fixture(["payback"]);
     const paybackStakingDeployment = await deployments.get("PaybackStaking");
+    const mockTokenDeployment = await deployments.get("MockToken");
     paybackStaking = await ethers.getContractAt(
       "PaybackStaking",
       paybackStakingDeployment.address
     );
+    mockToken = await ethers.getContractAt(
+      "MockToken",
+      mockTokenDeployment.address
+    );
+  });
+
+  describe("constructor", () => {
+    it("initializes the owner of the contract correctly", async () => {
+      const owner = await paybackStaking.owner();
+      assert.equal(deployer.address, owner);
+    });
+    it("initializes the APY correctly", async () => {
+      const apy = (await paybackStaking.APY()).toString();
+      assert.equal("10", apy);
+    });
+    it("initializes the token address correctly", async () => {
+      const tokenAddress = await paybackStaking.stakingToken();
+      assert.equal(tokenAddress, mockToken.address);
+    });
+    it("initializes the inactivity limit correctly", async () => {
+      const limit = await paybackStaking.inactivityLimit();
+      assert.equal("63113852", limit.toString());
+    });
   });
 });
