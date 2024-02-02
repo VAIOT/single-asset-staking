cc5ee958ed32cab785cbca5bbb6bad03cb851eb0
Mixowo <mixowo@hotmail.com>
2024-02-02 11:22:18 +0100

    Updated hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -75,8 +75,8 @@ const config: HardhatUserConfig = {
   },
   namedAccounts: {
     deployer: {
-      default: 0, // here this will by default take the first account as deployer
-      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+      default: 0,
+      1: 0,
     },
   },
 };
--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -17,10 +17,10 @@ export const networkConfig: networkConfigInfo = {
     blockConfirmations: 6,
   },
   polygon: {
-    stakingContractAddress: "0xD13cfD3133239a3c73a9E535A5c4DadEE36b395c",
-    rewardContractAddress: "0xD13cfD3133239a3c73a9E535A5c4DadEE36b395c",
-    feeCollectingAddress: "0x4FCD8Ba9AD619Db0e8116f363c2C71F1F146432A",
-    initialOwner: "0xA6261cFbB4cAe2369ab0Ce912D20ed2fC0B9fF0B",
+    stakingContractAddress: "", // Staking contract address
+    rewardContractAddress: "", // Rewards contract address
+    feeCollectingAddress: "", // Address that will be collecting fees
+    initialOwner: "", // Initial owner of the smart contract
     blockConfirmations: 6,
   },
 };
